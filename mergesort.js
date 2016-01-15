0b184732c6cbb43fe4fc4b35ae9893e9368ee637
Gustavo Ocasio <gocasiot@gmail.com>
2016-01-14 23:57:34 -0500

    Added additional test specs,fixed merge function and finished mergeSort function

--- mergesort.js
+++ mergesort.js
@@ -1,40 +1,64 @@
+
 function mergeSort(arr) {
-	return arr;
+	
+	if (arr.length === 0){
+		return [];
+	}
+	//base case
+	if (arr.length === 1){
+		return arr;
+	} 
+	var splitArrays = splitter(arr);
+	var firstHalf = mergeSort(splitArrays[0]);
+	
+	var secondHalf = mergeSort(splitArrays[1]);
+	
+
+	return merge(firstHalf, secondHalf);
 };
 
-function merge(arr1,arr2) {
+
+function merge (arr1, arr2){
 	var answer = [];
-	var maxLength = Math.max(arr1.length,arr2.length);
-	for (var i = 0; i < maxLength; i++) {
-		if (!arr1[i]) {
-			answer.push(arr2[i]);
-			console.log(answer);
+	var combinedLength = arr1.length + arr2.length;
+	while(combinedLength > answer.length){
+		if (arr1[0] !== undefined && arr2[0] !== undefined){
+			if(arr1[0] < arr2[0]){
+				answer.push(arr1.shift());
+			} else {
+				answer.push(arr2.shift());
+			}
+		} else if(!arr1[0]){
+			answer.push(arr2.shift());
+		} else {
+			answer.push(arr1.shift());
 		}
-		else if (!arr2[i]) {
-			answer.push(arr1[i]);
-		}
-		else if(arr1[i] > arr2[i]) {
-			answer.push(arr2[i]);
-			answer.push(arr1[i]);
-		}
-		else {
-			answer.push(arr1[i]);
-			answer.push(arr2[i]);
-		}
-	};
+	}
 	return answer;
-};
+}
 
 function splitter(arr) {
 	var arr1 = arr.slice(0, Math.ceil(arr.length/2));
 	var arr2 = arr.slice(Math.ceil(arr.length/2), arr.length);
 
-	return [arr1, arr2]
+	return [arr1, arr2];
 }
 
 
+// // THOUGHT PROCESS HERE:
+// // [[110, 33, 50, 2, 4, 3, 3]] ---> 1 array of 7 elements
 
-// if (array[i] > array[i+1]) {
-// 				var holder = array[i];
-// 				array[i] = array[i+1];
-// 				array[i+1] = holder;
+// // run: splitter(arr[0])
+// // [[110, 33, 50, 2],[4, 3, 3]]
+
+// // run: splitter(arr[0]), splitter(arr[1]) 
+// // [[110, 33], [50, 2], [4, 3], [3]]
+
+// // run: splitter(arr[0]), splitter(arr[1]), splitter(arr[2]), splitter(arr[3]) 
+// // [[110], [33], [50], [2], [4], [3], [3]]   ---> 7 arrays of 1 element
+
+
+
+
+
+
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -2,14 +2,23 @@ describe('Merge Sort', function(){
     it('handles an empty array', function(){
         expect( mergeSort([]) ).toEqual( [] );
     });
-    it("able to merge two arrays", function() {
-        expect(merge([4,5,20],[1,10,15])).toEqual([1,4,5,10,15,20]);
+    it("able to merge two sorted arrays", function() {
+        expect(merge([4,5,20,25],[1,10,15])).toEqual([1,4,5,10,15,20,25]);
+    });
+    it("able to merge two sorted arrays (w/1st bigger than 2nd), and more than 2 numbers from same array are pushed consecutively into results array", function() {
+        expect(merge([4,5,6,7,20,25],[1,10,15])).toEqual([1,4,5,6,7,10,15,20,25]);
+    });
+    it("able to merge two sorted arrays (w/2nd bigger than 1st), and more than 2 numbers from same array are pushed consecutive into results array", function() {
+        expect(merge([1,10,15],[4,5,6,7,20,25])).toEqual([1,4,5,6,7,10,15,20,25]);
     });
     it("able to split an even-numbered array into two arrays", function() {
         expect(splitter([1,4,5,10,15,20])).toEqual([[1,4,5],[10,15,20]]);
     });
     it('able to split an odd-numbered array into two arrays', function() {
-        expect(splitter([1,4,5,10,15,20,100])).toEqual([[1,4,5,10],[15,20,100]])
-    })
+        expect(splitter([1,4,5,10,15,20,100])).toEqual([[1,4,5,10],[15,20,100]]);
+    });
+    it('able to use mergeSort to sort array', function() {
+        expect(mergeSort([110, 33, 50, 2, 4, 3])).toEqual([2, 3, 4, 33, 50, 110]);
+    });
 });
 
